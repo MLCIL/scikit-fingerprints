f2187d41ff5378032aa886622442e65a5cccbad5
Jakub Adamczyk <jakubadamczyk10@gmail.com>
2026-06-05 15:39:38 +0200

    Fix tests

--- skfp/fingerprints/_new_mordred/descriptors/morse.py
+++ skfp/fingerprints/_new_mordred/descriptors/morse.py
@@ -7,7 +7,7 @@ from skfp.fingerprints._new_mordred.utils.atomic_properties import (
     get_sanderson_electronegativity,
     get_van_der_waals_volume,
 )
-from skfp.fingerprints._new_mordred.utils.graph_matrix import DistanceMatrix
+from skfp.fingerprints._new_mordred.utils.graph_matrix import DistanceMatrix3D
 
 """
 This code has been adapted from the BSD-licensed mordred-community library.
@@ -17,30 +17,24 @@ See skfp/fingerprints/data/mordred-community_bsd_license.txt for the license tex
 """
 
 # atomic properties used to weight distance matrices
-_PROPS_NAMES = [
-    "none",  # pure interatomic distance
-    "mass",
-    "van_der_Waals_volume",
-    "Sanderson_electronegativity",
-    "polarizability",
+_PROPS = [
+    ("unweighted", None),  # pure interatomic distance
+    ("mass", get_mass),
+    ("van_der_Waals_volume", get_van_der_waals_volume),
+    ("Sanderson_electronegativity", get_sanderson_electronegativity),
+    ("polarizability", get_polarizability),
 ]
-_PROPS_FUNCS = [
-    None,  # pure interatomic distance
-    get_mass,
-    get_van_der_waals_volume,
-    get_sanderson_electronegativity,
-    get_polarizability,
-]
-
 
 FEATURE_NAMES = [
-    f"MoRSE_{prop}_dist_{dist}" for prop in _PROPS_NAMES for dist in range(33)
+    f"MoRSE_{prop_name}_dist_{dist}"
+    for prop_name, prop_func in _PROPS
+    for dist in range(1, 33)
 ]
 
 
 @np.errstate(divide="ignore", invalid="ignore")
 def calc(
-    mol_3d: Mol, distance_matrix_3d: DistanceMatrix
+    mol_3d: Mol, distance_matrix_3d: DistanceMatrix3D
 ) -> tuple[np.ndarray, list[str]]:
     """
     MoRSE descriptors.
@@ -56,16 +50,16 @@ def calc(
         return np.full(160, np.nan, dtype=np.float32), FEATURE_NAMES
 
     values = []
-    for name, func in zip(_PROPS_NAMES, _PROPS_FUNCS, strict=True):
+    for name, func in _PROPS:
         for distance in range(1, 33):
-            if name == "none":
+            if name == "unweighted":
                 props = np.ones(num_atoms)
             else:
-                props = np.asarray([func(atom) for atom in atoms])
-                carbon_prop = func(Atom(6))
+                props = np.asarray([func(atom) for atom in atoms])  # type: ignore
+                carbon_prop = func(Atom(6))  # type: ignore
                 props = props / carbon_prop
 
-            props = props.reshape(-1, 1)
+            props = props.reshape(1, -1)
 
             if distance == 1:
                 n = np.ones((num_atoms, num_atoms), dtype=np.float32)
@@ -76,7 +70,7 @@ def calc(
 
             np.fill_diagonal(n, 0)
 
-            value = 0.5 * np.ravel(props @ n @ props.T)[0]
+            value = 0.5 * np.ravel(props.dot(n).dot(props.T))[0]
             values.append(value)
 
     return np.asarray(values, np.float32), FEATURE_NAMES
