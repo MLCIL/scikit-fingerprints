9234f7f48ac6ea4434e82f609687fb98a50fa801
Jakub Adamczyk <jakubadamczyk10@gmail.com>
2026-09-02 10:48:37 +0200

    Fix docstrings

--- skfp/fingerprints/_new_mordred/descriptors/rdkit_descriptors.py
+++ skfp/fingerprints/_new_mordred/descriptors/rdkit_descriptors.py
@@ -1,12 +1,3 @@
-"""
-Mordred descriptors implemented as direct RDKit wrappers.
-
-This code has been adapted from the BSD-licensed mordred-community library.
-https://github.com/JacksonBurns/mordred-community
-
-See skfp/fingerprints/data/mordred-community_bsd_license.txt for the license text.
-"""
-
 import numpy as np
 from rdkit.Chem import (
     Crippen,
@@ -20,6 +11,13 @@ from rdkit.Chem.EState import EState_VSA
 
 from skfp.fingerprints._new_mordred.utils.descriptor_evaluation import safe_value
 from skfp.fingerprints._new_mordred.utils.graph_matrix import DistanceMatrix
+
+"""
+This code has been adapted from the BSD-licensed mordred-community library.
+https://github.com/JacksonBurns/mordred-community
+
+See skfp/fingerprints/data/mordred-community_bsd_license.txt for the license text.
+"""
 
 FEATURE_NAMES_2D = [
     "BalabanJ",
